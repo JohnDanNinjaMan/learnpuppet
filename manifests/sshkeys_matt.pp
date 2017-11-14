3d017d335b4eab7b675216eddc2498f9c9ebe5ca
Matt Pickin <johndanninjaman@live.co.uk>
2017-11-14 22:39:26 +0000

    Added a manifest that adds ssh key to Ubuntu's authorized key list.

---
+++ manifests/sshkeys_matt.pp
@@ -0,0 +1,4 @@
+ssh_authorized_key { 'johndanninjaman@live.co.uk' :
+        user    => 'ubuntu',
+        type    => 'ssh-rsa',
+        key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDUTtE/21qGmFU4CmFIsy5MZMgyeVnKI/h1swCDO9IxG4RDwTdaHc16BKp0SPB+755SX/qGbZNyy0vPsCnLOIJZgnwyBy8qR3rQPBQ9TK+1UaMOWnUDe+SDGCxY46rOtgtJys00eUnMrfmFe6YyAhDjeS2cTTK2c+e5afaJFCg4vy9LhAxTxOAGOGN0aMN+SgaFmw4mnpUYRyEPVZ0OaC9vCMA1F6KORzlC2K2j504vW559kouIlsnI8jNfFutx0mar648xjAnm83xV6SD6KDMfWs43JWqrP3QhwX6VzVg2taesWLlJlEoT4utWnNp5mm7SDSclKLmStbdNOQTpELA9',
